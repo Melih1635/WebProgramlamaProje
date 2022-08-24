6dd41a7bab2b9de75814a4ced6bdf4e19d7efc51
Melih Özmen <63011591+Melih1635@users.noreply.github.com>
2022-08-24 13:23:46 +0300

    Proje Dosyaları

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,113 @@
+﻿using Odev.Models;
+using Odev.Models.DataContext;
+using Odev.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Odev.Controllers
+{
+    public class AdminController : Controller
+    {
+        AraçKiralamaDBContext db = new AraçKiralamaDBContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.BlogSay = db.Blog.Count();
+            ViewBag.KategoriSay = db.Kategori.Count();
+            ViewBag.UrunSay = db.Urun.Count();
+            ViewBag.YorumSay = db.Yorum.Count();
+
+            var sorgu = db.Kategori.ToList();
+            return View(sorgu);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin admin, string sifre)
+        {
+            var md5pass = Crypto.Hash(sifre, "MD5");
+            var login = db.Admin.Where(x => x.Eposta == admin.Eposta).SingleOrDefault();
+            if (login.Eposta==admin.Eposta && login.Sifre==md5pass)
+            {
+                Session["adminid"] = login.AdminId;
+                Session["eposta"] = login.Eposta;
+                Session["yetki"] = login.Yetki;
+                return RedirectToAction("Index","Admin");
+            }
+            ViewBag.Uyari = "Kullanıcı adı ya da şifre yanlış!";
+            return View(admin);
+        }
+
+        public ActionResult Logout()
+        {
+            Session["adminid"] = null;
+            Session["eposta"] = null;
+            Session.Abandon();
+            return RedirectToAction("Login", "Admin");
+        }
+
+        public ActionResult Adminler()
+        {
+            return View(db.Admin.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Admin admin, string sifre, string eposta)
+        {
+
+            if (ModelState.IsValid)
+            {
+                admin.Sifre = Crypto.Hash(sifre, "MD5");
+                db.Admin.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+            }
+            return View(admin);
+        }
+        public ActionResult Edit(int id)
+        {
+            var a = db.Admin.Where(x => x.AdminId == id).SingleOrDefault();
+            return View(a);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Admin admin, string sifre, string eposta)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var a = db.Admin.Where(x => x.AdminId == id).SingleOrDefault();
+                a.Sifre = Crypto.Hash(sifre, "MD5");
+                a.Eposta = admin.Eposta;
+                a.Yetki = admin.Yetki;
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+            }
+            return View(admin);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var a = db.Admin.Where(x => x.AdminId == id).SingleOrDefault();
+            if (a != null) 
+            {
+                db.Admin.Remove(a);
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using Odev.Models.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+using PagedList;
+using PagedList.Mvc;
+using Odev.Models.Model;
+
+namespace Odev.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AraçKiralamaDBContext db = new AraçKiralamaDBContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            ViewBag.Urunler = db.Urun.ToList().OrderByDescending(x => x.UrunId);
+
+            return View();
+        }
+
+        public ActionResult SliderPartial()
+        {
+            return View(db.Slider.ToList().OrderByDescending(x=>x.SliderId));
+
+        }
+        
+        public ActionResult UrunPartial()
+        {
+            return View(db.Urun.ToList());
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            
+
+            return View(db.Hakkimizda.SingleOrDefault());
+        }
+
+        public ActionResult Araclarimiz()
+        {
+            return View(db.Urun.ToList().OrderByDescending(x=>x.UrunId));
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View(db.Iletisim.SingleOrDefault());
+        }
+
+        [HttpPost]
+        public ActionResult Iletisim(string adsoyad = null, string email = null, string konu = null, string mesaj = null)
+        {
+            if (adsoyad != null && email != null)
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 25;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "ysfcvz7898@gmail.com";
+                WebMail.Password = "ysfemre123";
+                WebMail.Send("ysfcvz7898@gmail.com", konu, email, mesaj);
+                ViewBag.Uyari = "Mesajınız Başarıyla Gönderildi.";
+                
+            }
+            else
+            {
+                ViewBag.Uyari = "Hata Oluştu! Tekrar Deneyin.";
+            }
+            return View();
+        }
+
+        public ActionResult Blog(int Sayfa = 1)
+        {
+            return View(db.Blog.Include("Kategori").OrderByDescending(x=>x.BlogId).ToPagedList(Sayfa,4));
+        }
+
+        public ActionResult KategoriBlog(int id, int Sayfa = 1)
+        {
+            var b = db.Blog.Include("Kategori").OrderByDescending(x=>x.BlogId).Where(x => x.Kategori.KategoriId == id).ToPagedList(Sayfa, 5);
+            return View(b);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+            var b = db.Blog.Include("Kategori").Include("Yorums").Where(x => x.BlogId == id).SingleOrDefault();
+            return View(b);
+        }
+
+        public JsonResult YorumYap(string adsoyad, string eposta, string icerik, int blogid) 
+        {
+            if (icerik == null)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            db.Yorum.Add(new Yorum { AdSoyad = adsoyad, Eposta = eposta, Icerik = icerik, BlogId = blogid, Onay = false });
+            db.SaveChanges();
+            
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult BlogKategoriPartial()
+        {
+            return PartialView(db.Kategori.Include("Blogs").ToList().OrderBy(x => x.KategoriAd));
+        }
+
+        public ActionResult BlogKayitPartial()
+        {
+            return PartialView(db.Blog.ToList().OrderByDescending(x=>x.BlogId));
+        }
+
+        public ActionResult FooterPartial()
+        {
+            ViewBag.Urunler = db.Urun.ToList().OrderByDescending(x => x.UrunId);
+
+            ViewBag.Iletisim = db.Iletisim.SingleOrDefault();
+
+            ViewBag.Blog = db.Blog.ToList().OrderByDescending(x => x.BlogId);
+
+            return PartialView();   
+        }
+
+
+
+    }
+}
